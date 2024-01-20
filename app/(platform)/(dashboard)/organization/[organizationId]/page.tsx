26454eb7c1da21e58a894368e11e8c41f3d693ae
Simon Franco Angarita <zaimon88@gmail.com>
2024-01-20 06:43:12 -0500

    Board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,11 +1,9 @@
 import { db } from "@/lib/db";
-import { Board } from "./board";
-import Form from "./form";
 
-// import { Separator } from "@/components/ui/separator";
+import { Separator } from "@/components/ui/separator";
 
-// import { Info } from "./_components/info";
-// import { BoardList } from "./_components/board-list";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 // import { checkSubscription } from "@/lib/subscription";
 
 const OrganizationIdPage = async () => {
@@ -13,20 +11,13 @@ const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
   return (
     <div className="w-full mb-20">
-      <div className="flex flex-col space-x-4">
-        <Form />
-        <div className="space-y-2">
-          {boards.map((board) => (
-            <Board key={board.id} title={board.title} id={board.id} />
-          ))}
-        </div>
-      </div>
-      {/* <Info isPro={isPro} />
-      <Separator className="my-4" /> */}
+      <div className="flex flex-col space-x-4"></div>
+      <Info />
+      <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        {/* <Suspense fallback={<BoardList.Skeleton />}>
-          <BoardList />
-        </Suspense> */}
+        {/* <Suspense fallback={<BoardList.Skeleton />}> */}
+        <BoardList />
+        {/* </Suspense> */}
       </div>
     </div>
   );
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,4 +1,4 @@
-// import { Toaster } from "sonner";
+import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 
 // import { ModalProvider } from "@/components/providers/modal-provider";
@@ -7,9 +7,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      {/* <QueryProvider>
-        <Toaster />
-        <ModalProvider /> */}
+      {/* <QueryProvider> */}
+      <Toaster />
+      {/* <ModalProvider /> */}
       {children}
       {/* </QueryProvider> */}
     </ClerkProvider>
