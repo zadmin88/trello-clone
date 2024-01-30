dd85f4fd68db1bc32d09914d5640a3baea473737
Simon Franco Angarita <zaimon88@gmail.com>
2024-01-29 21:05:02 -0500

    Card modal

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,17 +1,17 @@
 import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 
-// import { ModalProvider } from "@/components/providers/modal-provider";
-// import { QueryProvider } from "@/components/providers/query-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      {/* <QueryProvider> */}
-      <Toaster />
-      {/* <ModalProvider /> */}
-      {children}
-      {/* </QueryProvider> */}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
